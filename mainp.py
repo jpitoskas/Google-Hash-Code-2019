4c71de5edcb771ce73acd2ef97a8db1b90bc33f1
jpitoskas <jpitoskas@gmail.com>
2019-02-28 23:08:56 +0200

    sda

--- mainp.py
+++ mainp.py
@@ -19,25 +19,84 @@ def get_number():
     except ValueError:
         return float(data)
 
-class photo:
-    def __init__(self, ori, num, tags):
+# class slide:
+#     def __init__(self)
+
+class hslide:
+    def __init__(self, ori, tags, id):
         self.ori = ori
-        self.num = num
         self.tags = tags
+        self.id = id
+        self.flag = True
+
+class vslide:
+    def __init__(self, ori, tags, ids):
+        self.ori = ori
+        self.tags = tags
+        self.ids = ids
+        self.flag = True
+
+class photo:
+    def __init__(self, ori, tags, id):
+        self.ori = ori
+        self.tags = tags
+        self.id = id
+
+def score(list1, list2):
+    t1 = len(list1)
+    t2 = len(list2)
+    t = len(list(set(list1).intersection(list2)))
+    return min(t1-t, t, t2-t)
+
+from random import shuffle
 
 N = get_number()
 h = []
 photos = []
 v = []
+hs = []
 for i in range(N):
     ori = get_word()
     num = get_number()
     tags = []
     for _ in range(num):
         tags.append(get_word())
-    photos.append(photo(ori, num, tags))
-    print(photos[i].ori, photos[i].num, photos[i].tags)
-    i
+    photos.append(photo(ori, tags, i))
+    if (ori == 'H'):
+        h.append(photo(ori, tags, i))
+        hs.append(hslide(ori, tags, i))
+    else:
+        v.append(photo(ori, tags, i))
+        # isos i+1
+    # print(photos[i].ori, photos[i].num, photos[i].tags, photos[i].id )
+
+shuffle(v)
+
+vs = []
+for i in range(0, len(v), 2):
+    tags = list(set(v[i].tags+v[i+1].tags))
+    ids = []
+    ids.append(v[i].id)
+    ids.append(v[i+1].id)
+    s = vslide('V', tags, ids)
+    vs.append(s)
+import sys
+slides = hs + vs
+shuffle(slides)
+file1 = 'e'
+file2 = '_output'
+out = open(file1 + file2 + ".txt", 'w')
+out.write(str(len(slides)) + '\n')
+for i in range(len(slides)):
+    if (slides[i].ori == 'H'):
+        out.write(str(slides[i].id) + '\n')
+    else:
+        out.write(str(slides[i].ids[0]) + ' ' + str(slides[i].ids[1]) + '\n')
+out.close()
+
+
+# print(score(['a','b'],['b','c','d']))
+# print(list(set(['a','b']+['b','c'])))
 
 
 # if (get_word() == 'H'):
